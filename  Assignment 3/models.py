b4a0c8c5547b36d59aea1d46a21fc15110bfaba4
Jan Rzepkowski 247777 <jan.rzepkowski21@gmail.com>
2025-01-03 20:01:44 +0100

    Using mapped_column() for defining columns in model

---  Assignment 3/models.py
+++  Assignment 3/models.py
@@ -1,13 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import mapped_column
 
 db = SQLAlchemy()
 
 class DataPoint(db.Model):
     __tablename__ = 'data_points'
-    id = db.Column(db.Integer, primary_key=True)
-    feature1 = db.Column(db.Float, nullable=False)
-    feature2 = db.Column(db.Float, nullable=False)
-    category = db.Column(db.Integer, nullable=False)
+    id = mapped_column(db.Integer, primary_key=True)
+    feature1 = mapped_column(db.Float, nullable=False)
+    feature2 = mapped_column(db.Float, nullable=False)
+    category = mapped_column(db.Integer, nullable=False)
 
     def __repr__(self):
         return f"<DataPoint id={self.id} feature1={self.feature1} feature2={self.feature2} category={self.category}>"
